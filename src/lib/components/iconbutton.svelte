f7ab560de62a7da26fe15e2e6be67d9f0cbed281
Elias-Larsson <aroezi05@gmail.com>
2025-08-21 22:28:29 +0200

    feat: add icon button

---
+++ src/lib/components/iconbutton.svelte
@@ -0,0 +1,18 @@
+
+<script lang="ts">
+    export let alt: string;
+    export let src: string;
+    export let href: string;
+</script>
+
+<a href={href}>
+    <img alt={alt} src={src} class="icon"/>
+</a>
+
+<style>
+.icon {
+    height: 40px;
+    width: 40px;
+    position: relative;
+}
+</style>

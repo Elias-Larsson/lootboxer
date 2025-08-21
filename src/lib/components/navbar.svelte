f2065c4219071a291ee3a7468714f1697dee8ccd
Elias-Larsson <aroezi05@gmail.com>
2025-08-21 22:30:02 +0200

    feat: add navbar component

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Icon from './iconbutton.svelte';
+</script>
+<nav class="bg-slate-700 rounded-2xl">
+	<ul class="flex flex-row items-center justify-center px-5 py-4 gap-6">
+		<li>
+			<Icon alt="trade icon" src="/tradeicon.svg" href="/#"/>
+		</li>
+        <!--Button for lootbox animation-->
+		<button><li class="text-xl px-10 py-2 mint-400 rounded-xl">Open Lootbox</li></button>
+		<li>
+			<Icon alt="hello" src="/profileicon.svg" href="/profile"/>
+		</li>
+	</ul>
+</nav>

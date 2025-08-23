b99e70516c2bc9c322b4054661f93fa25dc188df
Elias-Larsson <aroezi05@gmail.com>
2025-08-23 14:42:18 +0200

    feat: add balance component

---
+++ src/lib/components/balance.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	type Props = {
+		money?: number;
+	};
+	let { money }: Props = $props();
+</script>
+
+<div class="flex flex-row text-slate-400 text-xl absolute top-0 p-8">
+    <img alt="balance" src="coinicon.svg"/>
+    <p>{money ? money : 0}</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script>
 	import Navbar from '$lib/components/navbar.svelte';
+	import Balance from '$lib/components/balance.svelte'
 </script>
-
-<main class="flex w-full flex-col items-center justify-center h-screen gap-10">
-	<h1 class="font-bold text-slate-50">lootboxer</h1>
-    <img alt="Lootbox" src="/lootbox.svg"/>
-	<Navbar></Navbar>
+<main class="flex flex-col justify-center items-center h-screen p-8">
+	<Balance></Balance>
+	<div class="flex w-full flex-col items-center justify-center gap-10 ">
+		<h1 class="font-bold text-slate-50">lootboxer</h1>
+		<img alt="Lootbox" src="/lootbox.svg"/>
+		<Navbar></Navbar>
+	</div>
 </main>
